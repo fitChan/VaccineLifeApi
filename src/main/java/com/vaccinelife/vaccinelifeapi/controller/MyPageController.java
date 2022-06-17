9aaeed0d13843e5f494a73ee174632165349a580
fitChan <84306157+fitChan@users.noreply.github.com>
2022-06-18 00:25:47 +0900

    mypagecontroller take token information by using @RequestHeader

--- src/main/java/com/vaccinelife/vaccinelifeapi/controller/MyPageController.java
+++ src/main/java/com/vaccinelife/vaccinelifeapi/controller/MyPageController.java
@@ -5,11 +5,15 @@ import com.vaccinelife.vaccinelifeapi.exception.ApiException;
 import com.vaccinelife.vaccinelifeapi.security.JwtTokenProvider;
 import com.vaccinelife.vaccinelifeapi.service.*;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Spliterator;
 
 @AllArgsConstructor
 @RestController
@@ -21,21 +25,44 @@ public class MyPageController {
     private final MedicalService medicalService;
 
     //백신 후기 List 받기
-    @GetMapping("/{Token}/vacBoard")
-    public ResponseEntity<List<VacBoardSimRequestDto>> getMypageVacBoard(@PathVariable String Token) {
-        return ResponseEntity.ok().body(vacBoardService.getMypageVacBoard(Token));
+//    @GetMapping("/{Token}/vacBoard")
+//    public ResponseEntity<List<VacBoardSimRequestDto>> getMypageVacBoard(@PathVariable String Token) {
+//        return ResponseEntity.ok().body(vacBoardService.getMypageVacBoard(Token));
+//    }
+    @GetMapping("/mypage/vacBoard")
+    public ResponseEntity<List<VacBoardSimRequestDto>> getMypageVacBoard(@RequestHeader Map<String, String> data) {
+        String headerToken = data.get("Authorization");
+        String[] split = headerToken.split(" ");
+        String token = split[1];
+        return ResponseEntity.ok().body(vacBoardService.getMypageVacBoard(token));
     }
 
-    //자가 격리 List 받기
-    @GetMapping("/{Token}/quarBoard")
-    public ResponseEntity<List<QuarBoardSimRequestDto>> getMypageQuarBoard(@PathVariable  String Token){
-        return ResponseEntity.ok().body(quarBoardService.getMypageQuarBoard(Token));
+//    //자가 격리 List 받기
+//    @GetMapping("/{Token}/quarBoard")
+//    public ResponseEntity<List<QuarBoardSimRequestDto>> getMypageQuarBoard(@PathVariable  String Token){
+//        return ResponseEntity.ok().body(quarBoardService.getMypageQuarBoard(Token));
+//    }
+
+    @GetMapping(value = "/mypage/quarBoard")
+    public ResponseEntity<List<QuarBoardSimRequestDto>> getMypageQuarBoard(@RequestHeader Map<String, String> data){
+        String headerToken = data.get("Authorization");
+        String[] split = headerToken.split(" ");
+        String token = split[1];
+        return ResponseEntity.ok().body(quarBoardService.getMypageQuarBoard(token));
     }
 
+//    //의료진 분들께 한마디 List 받기
+//    @GetMapping("/{Token}/medical")
+//    public ResponseEntity<List<MedicalResponseDto>> getMypageMedical(@PathVariable  String Token){
+//        return ResponseEntity.ok().body(medicalService.getMypageMedical(Token));
+//    }
     //의료진 분들께 한마디 List 받기
-    @GetMapping("/{Token}/medical")
-    public ResponseEntity<List<MedicalResponseDto>> getMypageMedical(@PathVariable  String Token){
-        return ResponseEntity.ok().body(medicalService.getMypageMedical(Token));
+    @GetMapping("/mypage/medical")
+    public ResponseEntity<List<MedicalResponseDto>> getMypageMedical(@RequestHeader Map<String, String> data){
+        String headerToken = data.get("Authorization");
+        String[] split = headerToken.split(" ");
+        String token = split[1];
+        return ResponseEntity.ok().body(medicalService.getMypageMedical(token));
     }
 
 
